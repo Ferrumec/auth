13af3ae169ac1e6ea0e0e77aac98abd18ab0f59a
ochiecommerce <ochieaustine98@gmail.com>
2026-03-10 12:15:11 +0300

    documented migrations::run_migratins

--- src/migrations.rs
+++ src/migrations.rs
@@ -4,6 +4,11 @@ use sqlx::{
 };
 pub static MIGRATOR: Migrator = sqlx::migrate!("./migrations");
 
+/// Runs migration files of this module.
+/// Use this method if and only if this is the only sqlx migration you are running on your app.
+/// This is because sqlx will fail all successive migrations if they are like this one.
+/// If you have multiple migrations, copy all the migration files into one place and run them once using sql::Migrator::run.
+/// You can find the migration files of this module at ./migrations directory
 pub async fn run_migration(pool: &Pool<Sqlite>) -> Result<(), MigrateError> {
     MIGRATOR.run(pool).await
 }
